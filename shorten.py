b56b322833d4a44cd15c5c440ac8abb352074b2a
adamawoolley <adamawoolley@gmail.com>
2019-03-03 16:23:11 -0800

    Fixed type error

--- shorten.py
+++ shorten.py
@@ -1,22 +1,22 @@
 from flask import Flask, redirect, request, render_template
 from pymongo import MongoClient
 
+
 db = MongoClient('mongodb://localhost:27017/')['url-shortener']['urls']
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        print('bob')
-        print(request.form['url'])
-        print({'url':request.form['url']})
-        id = db.insert_one({'url': request.form['url']}).inserted_id
-        return render_template('url_shortener.html', url=id)
+        id = db.insert_one({'_id': db.count() + 1, 'url': request.form['url']}).inserted_id
+        return render_template('url_shortener.html', id=id)
     return render_template('url_shortener.html')
 
-@app.route('/<int:id>')
+@app.route('/<int:id>', methods=['GET'])
 def reroute(id):
-    return redirect(db.find({}, {'_id': id})['url'])
+    print(db.find({}, {'_id': id}))
+    for x in db.find({'_id': id}):
+        return redirect(x['url'])
 
 if __name__ == "__main__":
     app.run(debug=True)
