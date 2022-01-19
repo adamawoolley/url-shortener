b0f371e4517d969992444c31ddf1c3ac65194d5c
adamawoolley <adamawoolley@gmail.com>
2022-01-18 18:55:07 -0800

    removed mongodb integration

--- shorten.py
+++ shorten.py
@@ -1,22 +1,26 @@
 from flask import Flask, redirect, request, render_template
-from pymongo import MongoClient
+#from pymongo import MongoClient
 
 
-db = MongoClient('mongodb://localhost:27017/')['url-shortener']['urls']
+#db = MongoClient('mongodb://localhost:27017/')['url-shortener']['urls']
 app = Flask(__name__)
+urls = ['filler']
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        id = db.insert_one({'_id': db.count() + 1, 'url': request.form['url']}).inserted_id
+        #id = db.insert_one({'_id': db.count() + 1, 'url': request.form['url']}).inserted_id
+        id = len(urls)
+        urls.append(request.form['url'])
         return render_template('url_shortener.html', id=id)
-    return render_template('url_shortener.html')
+    return render_template('url_shortener.html', id=None)
 
 @app.route('/<int:id>', methods=['GET'])
 def reroute(id):
-    print(db.find({}, {'_id': id}))
-    for x in db.find({'_id': id}):
-        return redirect(x['url'])
+    #print(db.find({}, {'_id': id}))
+    return redirect(urls[id])
+    #for x in db.find({'_id': id}):
+    #    return redirect(x['url'])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=4998)
